8c2f12d76c08c84fdcbb9d0960c9de430e8807d8
Jan-Daniel Georgens <thinkJD@thinkjd.de>
2014-03-24 23:00:39 +0100

    Some typos removed new feature
    The player sprite has now two pictures for left and right.
    changed from relative paths to os.path.join

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import pygame
+import os.path
 
 class Player(pygame.sprite.Sprite):
     screen = None
@@ -6,12 +7,18 @@ class Player(pygame.sprite.Sprite):
     change_y = 20
     max_height = 0
     max_width = 0
-    movement = "undefined"
     rect = None
+    image_player_left = None
+    image_player_right = None
+    image_player_death = None
 
     def __init__(self, screen, pos_x, pos_y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("./Grapics/spriteSplayer.png")
+        # load pictures
+        base_path = os.path.dirname(__file__)
+        self.image_player_right = pygame.image.load(os.path.join(base_path, "Graphics/", "player_right.png"))
+        self.image_player_left = pygame.image.load(os.path.join(base_path, "Graphics/", "player_left.png"))
+        self.image = self.image_player_left
         self.rect = self.image.get_rect()
         self.rect.x = pos_x
         self.rect.y = pos_y
@@ -29,11 +36,13 @@ class Player(pygame.sprite.Sprite):
     def update(self):
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]:
+            self.image = self.image_player_left
             if self.rect.left - self.change_x >= 0:
                 self.rect.x -= self.change_x
             else:
                 self.rect.x = 0
         if key[pygame.K_RIGHT]:
+            self.image = self.image_player_right
             if self.rect.right + self.change_x <= self.max_width:
                 self.rect.x += self.change_y
             else:
